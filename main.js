3c4328984079e905830b08a9bf3315ece8eec031
inguinho da quebrada <107191882+ruivinho15@users.noreply.github.com>
2024-04-19 09:05:40 -0300

    ruivo

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual os melhor time?");
+    var respostaTime = prompt(" Qual a melhor Cantora?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
+    if (respostaTime.toLowerCase() === "Billie eilih pirate baird o'connell") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
